cbee4512f63a1ad2387b3bae7efc2b68e19fcb2e
Andrew Walker <sc18aw@leeds.ac.uk>
2022-02-26 21:39:42 +0000

    temporary fix to navbar home highlight issue (wrapped /home in two linkcontainers)

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -30,8 +30,10 @@ const Navigation = () => {
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
-                                <LinkContainer to="/home">
-                                    <Nav.Link>Home</Nav.Link>
+                                <LinkContainer to="/">
+                                    <LinkContainer to="/home">
+                                        <Nav.Link>Home</Nav.Link>
+                                    </LinkContainer>
                                 </LinkContainer>
                                 <LinkContainer to="/about">
                                     <Nav.Link>About us</Nav.Link>
