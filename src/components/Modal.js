3b36c323ab069e171c9c099fec5d300282e613cd
Andrew Walker <sc18aw@leeds.ac.uk>
2022-04-03 14:26:13 +0100

    added slider/opacity functionality in modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,78 +3,76 @@ import {useCallback, useContext, useMemo, useState} from "react";
 import Navbar from "../components/layout/Navbar";
 import * as React from "react";
 import RangeSlider from "./Slider";
-import {SetOpacityContext} from "./Contexts";
+import {ColourContext, OpacityContext, ReadabilityContext, SetColourContext, SetOpacityContext} from "./Contexts";
 
-export function SignIn(callback, deps) {
-    const [show, setShow] = useState(true);
-
+export function AccessibilityModal(callback, deps) {
+    // CONTEXTS
     const SetOpacity = useContext(SetOpacityContext)
+    const Opacity = useContext(OpacityContext)
+    const SetColour = useContext(SetColourContext)
+    const Colour = useContext(ColourContext)
 
-    const handleClick = () => {
-        setShow(!show);
-    };
-    const handleClose = () => setShow(false);
+    const Readability = useContext(ReadabilityContext)
 
 
-    const [colour, setColour] = useState(null)
-    // parent val for opacity slider, change variable names later!
-    const [parentVal, setParentVal] = useState(0.5);
-
+    // HANDLE OPACITY SLIDER
     const sliderValueChanged = useCallback((val) => {
         SetOpacity(val)
-        setParentVal(val);
     });
-
-    const colours = ['yellow', 'red', 'blue', 'green']
-
     const sliderProps = useMemo(
         () => ({
             min: 0,
             max: 0.9,
-            value: parentVal,
+            value: Opacity,
             step: 0.01,
             label: 'Colour Opacity',
             onChange: (e) => sliderValueChanged(e),
         }),
-        [parentVal]
+        [Opacity]
     );
 
+
+    // HANDLE OVERLAY COLOUR
+    const colours = ['yellow', 'red', 'blue', 'green']
     const renderButtons = colours => {
         return colours.map( (colour, index) => {
             return ( <li key={index}
                          className={'colour-selector ' + colour}
-                         onClick={() => setColour(colour)}>
+                         onClick={() => SetColour(colour)}>
             </li> )
         })
-
     }
 
 
+    // HANDLE MODAL LOGIC
+    const [show, setShow] = useState(false);
+    const handleClick = () => {setShow(!show)};
+    const handleClose = () => setShow(false);
 
     return (
         <div>
             <Navbar handleClick={handleClick} />
             <Modal show={show} onHide={handleClose}>
-                <Modal.Header>
+                <Modal.Header closeButton>
                     <Modal.Title>Accessibility Features</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     We've noticed you spent a long time on this page, would you like to activate any of the
-                    following features?
+                    following reading comprehension features?
                     <div id='toolbox'>
                         { renderButtons(colours) }
                     </div>
                     <div>
                         <RangeSlider {...sliderProps} />
                     </div>
-
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
+                    <Button variant="primary" onClick={handleClose}>
                         Close
                     </Button>
                 </Modal.Footer>
             </Modal>
+
         </div>
     );
 }
