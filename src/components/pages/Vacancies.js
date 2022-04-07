409251d9a5c7b9871aac98bb76cdda1f7c3bdd92
Andrew Walker <sc18aw@leeds.ac.uk>
2022-04-07 16:34:20 +0100

    fixed spelling mistake

--- src/components/pages/Vacancies.js
+++ src/components/pages/Vacancies.js
@@ -18,7 +18,7 @@ const Vacancies = () => {
         <Container style={pageStyle}>
             <h1>Work For Us</h1>
             <hr/>
-            <p>We do not have any jobs availabile right now.</p>
+            <p>We do not have any jobs available right now.</p>
             <br/>
             <br/>
             <br/>
